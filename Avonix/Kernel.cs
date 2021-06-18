c8768408154894132bcb6997992a00c74c6839bd
denver-code <csigorek@gmail.com>
2021-06-18 17:18:04 +0300

    Добавьте файлы проекта.

---
+++ Avonix/Kernel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace Avonix
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("AvoOS booted successfully!");
+        }
+
+        protected override void Run()
+        {
+            Console.Write("root@avoos:=> ");
+            var input = Console.ReadLine();
+            Console.WriteLine("avoh: Command not found!");
+            //Console.WriteLine(input);
+        }
+    }
+}
